fbf55ac4ee0640b704447530ac2d342bbb598acd
fz <natureorz@foxmail.com>
2016-10-24 20:28:39 +0800

    验证码upgrade

---
+++ src/main/java/xyz/fz/config/CustomConfiguration.java
@@ -0,0 +1,31 @@
+package xyz.fz.config;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+/**
+ * Created by Administrator on 2016/10/24 0024.
+ */
+@Configuration
+public class CustomConfiguration {
+
+    @Bean("googleKaptcha")
+    public DefaultKaptcha googleKaptcha() {
+
+        Properties properties = new Properties();
+        properties.setProperty("kaptcha.image.width", "150");
+        properties.setProperty("kaptcha.image.height", "60");
+        properties.setProperty("kaptcha.noise.color", "yellow");
+        properties.setProperty("kaptcha.textproducer.char.string", "1234567890");
+        properties.setProperty("kaptcha.textproducer.char.length", "4");
+        properties.setProperty("kaptcha.textproducer.font.color", "blue");
+        Config config = new Config(properties);
+        DefaultKaptcha kaptcha = new DefaultKaptcha();
+        kaptcha.setConfig(config);
+        return kaptcha;
+    }
+}
--- src/main/java/xyz/fz/controller/IndexController.java
+++ src/main/java/xyz/fz/controller/IndexController.java
@@ -1,5 +1,6 @@
 package xyz.fz.controller;
 
+import com.google.code.kaptcha.impl.DefaultKaptcha;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorController;
@@ -10,19 +11,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import xyz.fz.domain.user.TUser;
 import xyz.fz.service.user.UserService;
-import xyz.fz.utils.BaseUtil;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
 /**
  * Created by fz on 2016/9/15.
@@ -34,6 +31,9 @@ public class IndexController implements ErrorController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private DefaultKaptcha googleKaptcha;
 
     @RequestMapping("/")
     public String root() {
@@ -77,7 +77,7 @@ public class IndexController implements ErrorController {
     }
 
     @RequestMapping("/index/valCode")
-    public void valCode(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+    public void valCode(HttpServletResponse response, HttpSession session) {
 
         response.setHeader("Pragma", "No-cache");
         response.setHeader("Cache-Control", "no-cache");
@@ -85,43 +85,13 @@ public class IndexController implements ErrorController {
 
         ServletOutputStream sos = null;
         try {
-            int width = 60, height = 20;
-            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-            // 获取图形上下文
-            Graphics g = image.getGraphics();
-            // 生成随机类
-            Random random = new Random();
-            // 设定背景色
-            g.setColor(BaseUtil.getRandColor(200, 250));
-            g.fillRect(0, 0, width, height);
-            // 设定字体
-            g.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-            // 随机产生155条干扰线，使图象中的认证码不易被其它程序探测到
-            g.setColor(BaseUtil.getRandColor(160, 200));
-            for (int i = 0; i < 155; i++) {
-                int x = random.nextInt(width);
-                int y = random.nextInt(height);
-                int xl = random.nextInt(12);
-                int yl = random.nextInt(12);
-                g.drawLine(x, y, x + xl, y + yl);
-            }
-            // 取随机产生的认证码(4位数字)
-            String sRand = "";
-            for (int i = 0; i < 4; i++) {
-                String rand = String.valueOf(random.nextInt(10));
-                sRand += rand;
-                // 将认证码显示到图象中
-                // 调用函数出来的颜色相同，可能是因为种子太接近，所以只能直接生成
-                g.setColor(new Color(20 + random.nextInt(110), 20 + random.nextInt(110), 20 + random.nextInt(110)));
-                g.drawString(rand, 13 * i + 6, 16);
-            }
-            // 将认证码存入SESSION
-            session.setAttribute("valCode", sRand);
-            // 图象生效
-            g.dispose();
+            // 生成验证码
+            String capText = googleKaptcha.createText();
+            session.setAttribute("valCode", capText);
+            BufferedImage capImage = googleKaptcha.createImage(capText);
+            // 输出验证码到页面
             sos = response.getOutputStream();
-            // 输出图象到页面
-            ImageIO.write(image, "JPEG", sos);
+            ImageIO.write(capImage, "JPEG", sos);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
