c3769a09d9bd26d4b159627b424b01e2f7a8e45c
Fz <natureorz@foxmail.com>
2017-02-14 23:57:03 +0800

    upgrade

--- src/main/java/xyz/fz/controller/ExceptionAdviceController.java
+++ src/main/java/xyz/fz/controller/ExceptionAdviceController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
