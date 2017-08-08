04f9e16cc864a3b868ca424567db67b38156995b
fz <natureorz@foxmail.com>
2017-08-08 18:19:01 +0800

    upgrade

--- src/main/java/xyz/fz/Application.java
+++ src/main/java/xyz/fz/Application.java
@@ -3,11 +3,10 @@ package xyz.fz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @ServletComponentScan
-public class Application extends WebMvcConfigurerAdapter {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
