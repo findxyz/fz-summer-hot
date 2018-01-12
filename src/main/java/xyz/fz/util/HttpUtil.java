7dc797779d37c934ae86755296c1f54c326207de
fz <natureorz@foxmail.com>
2018-01-12 16:26:26 +0800

    okHttp 信任所有证书（不安全）

--- src/main/java/xyz/fz/util/HttpUtil.java
+++ src/main/java/xyz/fz/util/HttpUtil.java
@@ -4,7 +4,11 @@ import okhttp3.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.net.ssl.*;
+import java.io.File;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -22,22 +26,70 @@ public class HttpUtil {
 
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
+    public static final MediaType XML = MediaType.parse("application/xml; charset=utf-8");
+
+    private static final TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                }
+
+                @Override
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return new java.security.cert.X509Certificate[]{};
+                }
+            }
+    };
+
+    private static final SSLContext trustAllSslContext;
+
+    static {
+        try {
+            trustAllSslContext = SSLContext.getInstance("SSL");
+            trustAllSslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static final SSLSocketFactory trustAllSslSocketFactory = trustAllSslContext.getSocketFactory();
+
     private static OkHttpClient client = new OkHttpClient
             .Builder()
-            .connectTimeout(5, TimeUnit.SECONDS)
-            .readTimeout(5, TimeUnit.SECONDS)
-            .writeTimeout(5, TimeUnit.SECONDS)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .sslSocketFactory(trustAllSslSocketFactory, (X509TrustManager) trustAllCerts[0])
+            .hostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            })
             .build();
 
     public static String httpPostJson(String url, String json) throws IOException {
 
-        RequestBody requestBody = RequestBody.create(JSON, json);
+        return httpPostContent(url, JSON, json);
+    }
+
+    public static String httpPostXml(String url, String xml) throws IOException {
+
+        return httpPostContent(url, XML, xml);
+    }
+
+    private static String httpPostContent(String url, MediaType mediaType, String content) throws IOException {
+        RequestBody requestBody = RequestBody.create(mediaType, content);
         Request request = new Request
                 .Builder()
                 .url(url)
                 .post(requestBody)
                 .build();
-        logger.debug("Http PostJson Url: {}", request.url().toString());
+        logger.debug("Http PostContent Url: {}", request.url().toString());
         Response response = client.newCall(request).execute();
         return result(response);
     }
@@ -82,6 +134,30 @@ public class HttpUtil {
         return result(response);
     }
 
+    public static String httpUpload(String url, Map<String, Object> params) throws IOException {
+
+        MultipartBody.Builder formBodyBuilder = new MultipartBody.Builder().setType(MultipartBody.FORM);
+        for (Map.Entry entry : params.entrySet()) {
+            String key = entry.getKey().toString();
+            Object value = entry.getValue();
+            if (value instanceof File) {
+                File file = (File) value;
+                formBodyBuilder.addFormDataPart("file", file.getName(), RequestBody.create(MediaType.parse("image/jpg"), file));
+            } else {
+                formBodyBuilder.addFormDataPart(key, value.toString());
+            }
+        }
+        RequestBody formBody = formBodyBuilder.build();
+        Request request = new Request
+                .Builder()
+                .url(url)
+                .post(formBody)
+                .build();
+        logger.debug("Http Upload Url: {}", request.url().toString());
+        Response response = client.newCall(request).execute();
+        return result(response);
+    }
+
     private static String result(Response response) {
         if (response.isSuccessful()) {
             ResponseBody responseBody = response.body();
@@ -89,7 +165,7 @@ public class HttpUtil {
                 try {
                     return responseBody.string();
                 } catch (Exception e) {
-                    logger.error(e.getMessage());
+                    logger.error(BaseUtil.getExceptionStackTrace(e));
                     return "";
                 }
             } else {
